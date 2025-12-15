3e46dd943e77daa696f90e5e770ce05be85c443a
Heyder Medina <medinaheyder@gmail.com>
2025-12-14 21:23:44 -0600

    refactor: restructure App component layout and update background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import MiniCard from "./components/Card/MiniCard"
+import MiniCard from "./components/Card/MiniCard";
+import CardPrimary from "./components/Card/CardPrimary";
 
 function App() {
 
   return (
     <>
-    <div className="absolute inset-0 -z-5 w-full h-full">
-      <img src="/sss.jpg" alt="Background"
-      className="object-cover w-full " />
-    </div>
-    <div className="flex gap-2 p-24">
-      <MiniCard text="Hola, soy una tarjeasdasd a ad asd  ads ad  s as seda deas deas dsa sdasdasdta!" />
-      <MiniCard text="Hola, soy una tarjeta!" brightness={10} darkness={50} />
-      <MiniCard text="Hola, soy una tarjeta!" />
+      <div className="h-screen w-full">
+        <div className="absolute inset-0 -z-5">
+          <img src="/bgDemo.jpg" alt="Background"
+            className="object-cover w-full h-full" />
+        </div>
+        <div className="flex gap-2 p-24">
+          <MiniCard text="Hola, soy una tarjeasdasd a ad asd  ads ad  s as seda deas deas dsa sdasdasdta!" />
+          <CardPrimary text="Hola, soy una tarjeta!" />
+        </div>
       </div>
     </>
   )
