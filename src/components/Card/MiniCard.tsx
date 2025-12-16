2e423c239228c4fef854974b0a986de415173c04
Heyder Medina <medinaheyder@gmail.com>
2025-12-15 19:23:46 -0600

    refactor: shrink adjustement to minicard layout

--- src/components/Card/MiniCard.tsx
+++ src/components/Card/MiniCard.tsx
@@ -2,25 +2,33 @@ import { useClamp } from "../../hooks/UseClamp";
 
 export interface MiniCardProps {
   text: string;
+  Icon: string;
   brightness?: number;
   darkness?: number;
 }
 
-
-export function MiniCard({ text, brightness = 10, darkness = 10 }: MiniCardProps) {
-    const safeBrightness = useClamp(brightness, 0, 100, "brightness");
+export function MiniCard({ text, brightness = 10, darkness = 10, Icon }: MiniCardProps) {
+    const safeBrightness = useClamp(brightness, 0, 100);
     const bgBrigtness = safeBrightness / 100;
 
-    const safeDarkness = useClamp(darkness, 0, 100, "darkness");
+    const safeDarkness = useClamp(darkness, 0, 100);
     const bgDarkness = safeDarkness / 100;
 
     return (
         <div
             style={{ backgroundColor: `rgba(255,255,255,${bgBrigtness})` }}
-            className="inline-flex max-w-80 h-50 flex-col gap-4 p-8 
+            className="inline-flex shrink-0 max-w-80 h-50 flex-col gap-4 p-8
             rounded-4xl ring-1 ring-inset ring-white/20 backdrop-blur-xl shadow-xl overflow-hidden"
         >
+
+            {
+                Icon && (
+                    <Icon></Icon>
+                )
+            }
             <p className="text-2xl font-medium text-white text-pretty line-clamp-3">{text}</p>
+
+            {/* black layer */}
             <div className="absolute inset-0 -z-2 bg-black/15"
             style={{ backgroundColor: `rgba(0,0,0,${bgDarkness})`}}></div>
         </div>
