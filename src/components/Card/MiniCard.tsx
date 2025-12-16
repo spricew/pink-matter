fbb7453bbb68dedfd4e6a91b5167a243a98e1c40
Heyder Medina <medinaheyder@gmail.com>
2025-12-15 21:48:22 -0600

    fix: update Icon prop type in MiniCard component and adjust layout

--- src/components/Card/MiniCard.tsx
+++ src/components/Card/MiniCard.tsx
@@ -1,11 +1,14 @@
 import { useClamp } from "../../hooks/UseClamp";
 
+import type { ComponentType } from "react";
+
 export interface MiniCardProps {
   text: string;
-  Icon: string;
+  Icon: ComponentType<{ className?: string }>;
   brightness?: number;
   darkness?: number;
 }
+
 
 export function MiniCard({ text, brightness = 10, darkness = 10, Icon }: MiniCardProps) {
     const safeBrightness = useClamp(brightness, 0, 100);
@@ -17,16 +20,13 @@ export function MiniCard({ text, brightness = 10, darkness = 10, Icon }: MiniCar
     return (
         <div
             style={{ backgroundColor: `rgba(255,255,255,${bgBrigtness})` }}
-            className="inline-flex shrink-0 max-w-80 h-50 flex-col gap-4 p-8
+            className="inline-flex shrink-0 min-w-70 max-w-84 w-fit max-h-52 min-h-44 h-fit flex-col gap-4 p-8
             rounded-4xl ring-1 ring-inset ring-white/20 backdrop-blur-xl shadow-xl overflow-hidden"
         >
 
-            {
-                Icon && (
-                    <Icon></Icon>
-                )
-            }
-            <p className="text-2xl font-medium text-white text-pretty line-clamp-3">{text}</p>
+            <Icon className="size-12 text-white" />
+
+            <p className="text-xl font-medium text-white text-pretty line-clamp-3">{text}</p>
 
             {/* black layer */}
             <div className="absolute inset-0 -z-2 bg-black/15"
