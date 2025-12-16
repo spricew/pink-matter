d613d38e27872a4e3ade342401314df2acda0d24
Heyder Medina <medinaheyder@gmail.com>
2025-12-15 22:22:11 -0600

    feat: add PrimaryCard component to App with extended text example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { MiniCard } from "./components/Card/MiniCard";
+import { PrimaryCard } from "./components/Card/PrimaryCard";
 import React from "./components/ReactIcon";
 
 function App() {
@@ -11,8 +12,15 @@ function App() {
             className="object-cover w-full h-full" />
         </div>
         <div className="flex flex-wrap gap-2 p-24 h-full text-wrap">
-          <MiniCard text="Hello world!" 
-          Icon={React}/>
+          <MiniCard 
+            text="Hello world!"
+            Icon={React} />
+
+          <PrimaryCard
+            title="Hola mundo"
+            text="Esta Esta Esta Esta Esta Esta Esta Esta Esta Esta Esta Esta Esta asd asd ads asd asd asd ads saas d asd ad asd ad asd asdasd asd asd sd asd as dasd asd asd as dasd ads saas d asd ad asd ad asd asdasd asd asd sd asd as dasd asd asd as dasd ads saas d asd ad asd ad asd asdasd asd asd sd asd as dasd asd asd as dasd ads saas d asd ad asd ad asd asdasd asd asd sd asd as dasd asd asd as dasd ads saas d asd ad asd ad asd asdasd asd asd sd asd as dasd asd asd as dasd ads saas d asd ad asd ad asd asdasd asd asd sd asd as dasd asd asd as d es una tarjeta primaria de ejemplo con un texto un poco más largo para demostrar el comportamiento de la tarjeta cuando el contenido es extenso."
+            Icon={React} />
+
         </div>
       </div>
     </>
