d02a9fe3a9685129968d5f16529c4a357ad4cf2a
Heyder Medina <medinaheyder@gmail.com>
2025-12-15 22:22:04 -0600

    feat: export PrimaryCard and its props from Card component

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -1,2 +1,5 @@
 export { MiniCard } from "./MiniCard";
 export type { MiniCardProps } from "./MiniCard";
+
+export { PrimaryCard } from "./PrimaryCard";
+export type { PrimaryCardProps } from "./PrimaryCard";
